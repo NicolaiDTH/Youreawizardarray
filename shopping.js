8a40b497fcd349b45e3f0d3a4f3b2cf7e5dee873
Nicolai <nicolai@mmg.io>
2017-01-10 14:07:40 +0000

    additional function to make it an ordered list

--- shopping.js
+++ shopping.js
@@ -1,5 +1,21 @@
-var shoppingList = ['beans', 'cyanide', 'fruit shoot'];
+var shoppingList = [
+ 'beans', 
+ 'cyanide', 
+ 'fruit shoot'
+ ];
+
 shoppingList.pop();
 shoppingList.push('a camel');
 shoppingList.unshift('depression');
 shoppingList.shift();
+
+function printList (list) {
+ var listHTML = '<ol>';
+ for (var i = 0; i < list.length; i += 1) {
+  listHTML += '<li>' + list[i] + '</li>';
+ }
+ listHTML += '</ol>';
+ print(listHTML);
+}
+
+printList(shoppingList);
